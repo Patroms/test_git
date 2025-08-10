40e6c61bfa42444e8aa5ad8765f40ec88f810b84
admin <admin@WIN-8E95N3BRK0D>
2025-08-10 21:47:43 +0300

    Добавьте файлы проекта.

---
+++ FirstApp1/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpEssentials
+{
+    public class Counter
+    {
+        int cnt = 0;
+        public void Increment()
+        {
+            cnt++;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Текущее значение счетчика = " + cnt);
+        }
+        public void Reset()
+        {
+            cnt = 0;
+        }
+        public void StartFrom (int a)
+        {
+            cnt = a;
+        }
+    }
+}
---
+++ FirstApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Security.Policy;
+using static CSharpEssentials.Program;
+
+namespace CSharpEssentials
+{
+
+    class 
+    {
+        public class User
+        {
+
+        }
+
+        static void Main()
+        {
+            Character character = new Character();
+            character.equipment.items[0].ShowItem();//Меч:10()
+            character.equipment.items[1].ShowItem();//Лук:5()
+            Console.WriteLine($"Общий вес: {character.equipment.GetWeightOfEquipment()}");//Общий вес: 15
+        }
+    }
+}
