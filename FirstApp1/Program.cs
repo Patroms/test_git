cea610f6e654fcb8dbf54f0a70e36bbec2ef576a
user <user@WIN-OMVGI2R4Q8D>
2025-08-10 22:15:23 +0300

    Подготовлен файл к отправке

--- FirstApp1/Program.cs
+++ FirstApp1/Program.cs
@@ -1,24 +1,11 @@
 ﻿using System;
-using System.Data.SqlTypes;
-using System.Security.Policy;
-using static CSharpEssentials.Program;
-
 namespace CSharpEssentials
 {
-
-    class 
+    class Program
     {
-        public class User
-        {
-
-        }
-
         static void Main()
         {
-            Character character = new Character();
-            character.equipment.items[0].ShowItem();//Меч:10()
-            character.equipment.items[1].ShowItem();//Лук:5()
-            Console.WriteLine($"Общий вес: {character.equipment.GetWeightOfEquipment()}");//Общий вес: 15
+
         }
     }
 }
